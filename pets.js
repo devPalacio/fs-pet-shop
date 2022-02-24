7264d5649a1134a9e19a732ed61fbc0d55758fa2
Jay Palacio <jaypalacio@gmail.com>
2022-02-24 09:39:30 -0500

    pet shop with fs complete

--- pets.js
+++ pets.js
@@ -1,16 +1,66 @@
 "use strict";
 const fs = require("fs");
+const command = process.argv?.[2]?.toLowerCase();
+const commands = { read: null, create: null, update: null, destroy: null };
 
-console.log(process.argv);
-if (process.argv.length < 3) {
+if (process.argv.length < 3 || !(command in commands)) {
   console.error("Usage: node pets.js [read | create | update | destroy]");
   process.exit(1);
 }
-if (process.argv[2].toLowerCase() === "read") {
-  fs.readFile("pets.json", "utf8", function (error, data) {
-    if (error) {
-      return console.error(error);
+
+const index = process.argv[3];
+let petData;
+
+try {
+  petData = JSON.parse(fs.readFileSync("pets.json", "utf8"));
+} catch (err) {
+  return console.error(err);
+}
+
+if (command === "read") {
+  read();
+}
+
+if (command === "create") {
+  create();
+}
+
+function read() {
+  if (index !== undefined) {
+    if (!petData[+index]) {
+      console.error(`Usage: node pets.js read INDEX (index out of bounds)`);
+      process.exit(1);
     }
-    console.log(JSON.parse(data));
+    return console.log(petData[+index]);
+  }
+  return console.log(petData);
+}
+
+function create() {
+  if (process.argv.length < 6) {
+    console.error("Usage: node pet.js create AGE KIND NAME");
+    process.exit(1);
+  }
+  let newPet = {};
+  newPet.age = +process.argv[3];
+  newPet.kind = process.argv[4];
+  newPet.name = process.argv[5];
+  petData.push(newPet);
+  fs.writeFile("pets.json", JSON.stringify(petData, "utf8"), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.info(`Wrote ${newPet} to file`);
   });
 }
+
+//function readFile() {
+//  fs.readFile(process.argv[1], "utf8", function (error, data) {
+//    if (error) {
+//      return console.error(error);
+//    }
+//
+//    return data;
+//  });
+//}
