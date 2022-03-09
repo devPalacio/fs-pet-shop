e41a1f64156c495bd4b4f6ca9383040536fd070e
Jay Palacio <jaypalacio@gmail.com>
2022-03-09 19:52:20 +0000

    .env params and finished routes

--- restfulPsql.js
+++ restfulPsql.js
@@ -1,20 +1,16 @@
 "use strict";
-
 const fs = require("fs");
 const express = require("express");
 const morgan = require("morgan");
+const pg = require("pg")
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT ?? 8000;
-
-function readFile(callback) {
-  fs.readFile("pets.json", "utf-8", (err, data) => {
-    if (err) {
-      callback(err);
-    }
-    callback(null, JSON.parse(data));
-  });
-}
+const pool = new pg.Pool({
+  database: 'petshop'
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,113 +18,41 @@ app.use(morgan("dev"));
 
 app.get("/pets/:index", (req, res) => {
   let index = req.params.index;
-  readFile((err, petData) => {
-    if (err) {
-      res.status(500).send("Problem reading pets.json");
-      return;
-    }
-    if (!index || !petData[index]) {
-      res.status(404).send("Not found");
-      return;
-    }
-    res.setHeader("Content-type", "application/json");
-    res.json(petData[index]);
-  });
+  pool.query("SELECT * FROM pets WHERE id=$1", [index])
+    .then(result => res.json(result.rows[0]))
+    .catch(err => console.error(err))
 });
 
 app.get("/pets", (req, res) => {
-  readFile((err, petData) => {
-    if (err) {
-      res.status(500).send("Problem reading pets.json");
-      return;
-    }
-
-    res.setHeader("Content-type", "application/json");
-    res.json(petData);
-  });
+  pool.query('SELECT * FROM pets')
+    .then(result => res.json(result.rows))
 });
 
 app.post("/pets", (req, res) => {
-  readFile((err, petData) => {
-    if (err) {
-      res.status(500).send("Problem reading pets.json");
-      return;
-    }
-    let { age, name, kind } = req.body;
-    if (name === "" || kind === "" || !Number.isInteger(age)) {
-      res.status(400).send("Bad Request");
-      return;
-    }
-
-    petData.push(req.body);
-    fs.writeFile("pets.json", JSON.stringify(petData), "utf8", (err) => {
-      if (err) {
-        res.status(500).send("Problem writing to pets.json");
-        return;
-      }
-      res.setHeader("Content-type", "application/json");
-      res.send(req.body);
-    });
-  });
+  const { name, age, kind } = req.body
+  pool.query('INSERT INTO pets (name, age, kind) VALUES ($1,$2,$3)', [name, age, kind])
+    .then(result => res.json(req.body))
 });
 
 app.patch("/pets/:index", (req, res) => {
   let index = req.params.index;
-  readFile((err, petData) => {
-    if (err) {
-      res.status(500).send("Problem reading pets.json");
-      return;
-    }
-    if (!index || !petData[index]) {
-      res.status(404).send("Not Found");
-      return;
-    }
-
-    // patch logic
-    if (req.body.name) {
-      petData[index].name = req.body.name;
-    }
-    if (req.body.age && Number.isInteger(req.body.age)) {
-      petData[index].age = req.body.age;
-    }
-    if (req.body.kind) {
-      petData[index].kind = req.body.kind;
-    }
-
-    fs.writeFile("pets.json", JSON.stringify(petData), "utf8", (err) => {
-      if (err) {
-        res.status(500).send("Problem writing to pets.json");
-        return;
-      }
-      res.send(JSON.stringify(petData[index]));
-    });
-  });
+  const { name, age, kind } = req.body
+  const patchQ = ` 
+    UPDATE pets SET
+      name = COALESCE($1, name),
+      age = COALESCE($2, age),
+      kind = COALESCE($3, kind)
+    WHERE id = $4
+    RETURNING *
+    `
+  pool.query(patchQ, [name, age, kind, index])
+    .then(result => res.json(result))
 });
 
 app.delete("/pets/:index", (req, res) => {
-  let index = req.params?.index;
-  let removed;
-  readFile((err, petData) => {
-    if (err) {
-      res.status(500).send("Problem reading pets.json");
-      return;
-    }
-    if (!index || !petData[index]) {
-      res.status(404).send("Not found, pet does not exist at that index.");
-      return;
-    }
-    removed = petData[index];
-    petData = petData.slice(0, index).concat(petData.slice(index + 1));
-
-    fs.writeFile("pets.json", JSON.stringify(petData), "utf8", (err) => {
-      if (err) {
-        res.status(500).send("Problem writing to pets.json");
-        return;
-      }
-      res.setHeader("Content-type", "application/json");
-      res.json(removed);
-    });
-  });
+  let index = req.params.index;
+  pool.query('DELETE FROM pets WHERE id = $1 RETURNING *', [index])
+    .then(result => res.json(result.rows[0]))
 });
 
 app.listen(port, () => {
