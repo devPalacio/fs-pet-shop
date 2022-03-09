9a0ffa0546a4a1e737afcacc5c144a4bab206bfa
Jay Palacio <jaypalacio@gmail.com>
2022-03-09 11:16:30 -0500

    migration file

---
+++ migration.sql
@@ -0,0 +1,12 @@
+DROP TABLE if exists pets;
+
+CREATE TABLE pets (
+  id serial primary key,
+  name text,
+  age integer,
+  kind text,
+);
+
+insert into pets (name, age, kind) VALUES
+  ("fido", 7 ,"dog"),
+  ("Buttons", 5, "snake");
